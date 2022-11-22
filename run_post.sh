9eba507952ebec3404152b4097b0d6d3faca8d98
Stylianos Vassiliou <63289585+svassi04@users.noreply.github.com>
2022-11-22 15:34:13 +0200

    custom run script

--- run_post.sh
+++ run_post.sh
@@ -1,19 +1,19 @@
 
-
 #!/bin/bash
 o=$1
 for (( c=0 ; c<$1 ; c++ ));
 do
-	for (( i=0 ; i<5 ; i++ ));
-	do
+        for (( i=0 ; i<1 ; i++ ));
+        do
  #               python3 ./profiler/profiler.py -n node$c start
 #done
-	recs=$c*1000
-        ./wrk2/wrk -D exp -t 2 -c 2 -d 30 -s ./wrk2/scripts/social-network/compose-post.lua http://localhost:8080/wrk2-api/post/compose -R $recs >> files$c
+d=$(($(($c+1))*100))
+echo $d
+        ./wrk2/wrk -D exp -t 2 -c 2 -d 30 -s ./wrk2/scripts/social-network/compose-post.lua http://localhost:8080/wrk2-api/post/compose -R $d >> myOutput
 
        # ./wrk2/wrk -D exp -t 2 -c 2 -d $3 -L -s ./wrk2/scripts/social-network/mixed-workload.lua http://localhost:8080/wrk2-api/post/compose -R $2 >> file1
 
-	done
+        done
 #for (( c=0 ; c<$1 ; c++ ));
 #do
 #                python3 ./profiler/profiler.py -n node$c stop
@@ -22,6 +22,3 @@ do
 #do
 #               python3 ./profiler/profiler.py -n node$c report -d /tmp/data/node$c
 done
-
-
-
