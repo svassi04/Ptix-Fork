115d0ac82532a44c3541d7c0167d2f668f899958
Stylianos Vassiliou <63289585+svassi04@users.noreply.github.com>
2023-01-27 01:35:55 +0200

    run home

---
+++ run_home.sh
@@ -0,0 +1,25 @@
+
+#!/bin/bash
+o=$1
+rm myOutput
+for (( c=0 ; c<$1 ; c++ ));
+do
+        for (( i=0 ; i<10 ; i++ ));
+        do
+ #               python3 ./profiler/profiler.py -n node$c start
+#done
+d=$(($(($c+1))*100))
+echo $d
+        #./wrk2/wrk -D exp -t 2 -c 2 -d 30 -s ./wrk2/scripts/social-network/compose-post.lua http://localhost:8080/wrk2-api/post/compose -R $d >> myOutput
+        ./wrk2/wrk -D exp -t 2 -c 2 -d 30 -s ./wrk2/scripts/social-network/read-user-timeline.lua http://localhost:8080/wrk2-api/user-timeline/read -R $d >> myOutput
+       # ./wrk2/wrk -D exp -t 2 -c 2 -d $3 -L -s ./wrk2/scripts/social-network/mixed-workload.lua http://localhost:8080/wrk2-api/post/compose -R $2 >> file1
+
+        done
+#for (( c=0 ; c<$1 ; c++ ));
+#do
+#                python3 ./profiler/profiler.py -n node$c stop
+#done
+#for (( c=0 ; c<$1 ; c++ ));
+#do
+#               python3 ./profiler/profiler.py -n node$c report -d /tmp/data/node$c
+done
