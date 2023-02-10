526d23d5b068c403939532a76fa4b3225a273724
Stylianos Vassiliou <63289585+svassi04@users.noreply.github.com>
2023-02-10 14:36:50 +0200

    change folder location of outputs

--- run_home.sh
+++ run_home.sh
@@ -11,10 +11,10 @@ do
                 d=$(($(($c+1))*100))
                 echo $d
         #./wrk2/wrk -D exp -t 2 -c 2 -d 30 -s ./wrk2/scripts/social-network/compose-post.lua http://localhost:8080/wrk2-api/post/compose -R $d >> myOutput
-                python3 ./profiler/profiler.py -n node0 start
+                sudo python3 ./profiler/profiler.py -n node0 start
                 ./wrk2/wrk -D exp -t 2 -c 2 -d 30 -s ./wrk2/scripts/social-network/read-user-timeline.lua http://localhost:8080/wrk2-api/user-timeline/read -R $d >> myOutput
-                python3 ./profiler/profiler.py -n node0 stop
-                python3 ./profiler/profiler.py -n node0 report -d /tmp/data/qps$d/repeat$i
+                sudo python3 ./profiler/profiler.py -n node0 stop
+                sudo python3 ./profiler/profiler.py -n node0 report -d ~/temp/qps$d/repeat$i
        # ./wrk2/wrk -D exp -t 2 -c 2 -d $3 -L -s ./wrk2/scripts/social-network/mixed-workload.lua http://localhost:8080/wrk2-api/post/compose -R $2 >> file1
 
         done
