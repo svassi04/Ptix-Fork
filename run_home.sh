617fdd9ed0f2b5b9dae97015c55f94a9a57d97b6
Stylianos Vassiliou <63289585+svassi04@users.noreply.github.com>
2023-02-16 16:26:03 +0200

    compine cstate in one file and power in another

--- run_home.sh
+++ run_home.sh
@@ -14,10 +14,17 @@ do
                 sudo python3 ./profiler/profiler.py -n node0 start
                 ./wrk2/wrk -D exp -t 2 -c 2 -d 30 -s ./wrk2/scripts/social-network/read-user-timeline.lua http://localhost:8080/wrk2-api/user-timeline/read -R $d >> myOutput
                 sudo python3 ./profiler/profiler.py -n node0 stop
-                sudo python3 ./profiler/profiler.py -n node0 report -d ~/temp/qps$d/repeat$i
+                sudo python3 ./profiler/profiler.py -n node0 report -d ~/temp/node/qps$d/repeat$i
+				python3 ./profiler/analyze.py ~/temp/qps$d/repeat$i/ 0 39 30 >> cstateAv
+				cat ~/temp/qps$d/repeat$i/dram >> power
+				cat ~/temp/qps$d/repeat$i/package-0 >> power
+				cat ~/temp/qps$d/repeat$i/package-1 >> power
+				echo '-----' >> power
        # ./wrk2/wrk -D exp -t 2 -c 2 -d $3 -L -s ./wrk2/scripts/social-network/mixed-workload.lua http://localhost:8080/wrk2-api/post/compose -R $2 >> file1
 
         done
+		echo 'repeat end' >> cstateAv
+		echo 'repeat end' >> power
 #for (( c=0 ; c<$1 ; c++ ));
 #do
 #                python3 ./profiler/profiler.py -n node$c stop
