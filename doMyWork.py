b54451560b120dba33ca729ff8dc8290bbe02449
Stylianos Vassiliou <63289585+svassi04@users.noreply.github.com>
2023-03-23 21:09:22 +0200

    2 nodes

--- doMyWork.py
+++ doMyWork.py
@@ -1,8 +1,14 @@
 import numpy as np
 import csv
 import matplotlib.pyplot as plt
+from glob import glob
+#import os
+#print (glob("/output/*/", recursive = True))
+qpsRate=1
 
-f=open("output.txt", "r")
+
+for name in glob("./outputs/default_flags_smt_disabled_100-1000qps/runData"):
+    f=open(name, "r")
 next(f)
 next(f)
 next(f)
@@ -26,11 +32,11 @@ for i in range(len(data_array)):
         if (data_array[i][1]!="-nanus"):
             tmp = str(data_array[i][1])
             if(tmp.find("ms")!=-1):
-                AvLatency.append(float(str(data_array[i][1]).replace("ms",""))/1000)
+                AvLatency.append(float(str(data_array[i][1]).replace("ms","")))
             elif(tmp.find("us")!=-1):
-                AvLatency.append(float(str(data_array[i][1]).replace("us",""))/1000000)
+                AvLatency.append(float(str(data_array[i][1]).replace("us",""))/1000)
             elif(tmp.find("s")!=-1):
-                AvLatency.append(float(str(data_array[i][1]).replace("s","")))
+                AvLatency.append(float(str(data_array[i][1]).replace("s",""))*1000)
             else:
                 print("error")
         else:
@@ -40,11 +46,11 @@ for i in range(len(data_array)):
         if (data_array[i][3]!="-nanus"):
             tmp = str(data_array[i][3])
             if(tmp.find("ms")!=-1):
-                TailLatency.append(float(str(data_array[i][3]).replace("ms",""))/1000)
+                TailLatency.append(float(str(data_array[i][3]).replace("ms","")))
             elif(tmp.find("us")!=-1):
                 TailLatency.append(float(str(data_array[i][3]).replace("us",""))/1000000)
             elif(tmp.find("s")!=-1):
-                TailLatency.append(float(str(data_array[i][3]).replace("s","")))
+                TailLatency.append(float(str(data_array[i][3]).replace("s",""))*1000)
             else:
                 print("error")
        #AvLatency.append(str(data_array[i][1]).replace("ms",""))
@@ -57,10 +63,10 @@ print(len(AvLatency), len(TailLatency), len(reqpersec), len(totalreq), "\n")
 print("AvLatency:", AvLatency, "\n\nTailLatency:", TailLatency, "\n\nreqpersec:", reqpersec, "\n\ntotalreq:", totalreq)
 l=0
 #plt.plot(AvLatency,reqpersec)
-for i in range(100,1100,100):
+for i in range(qpsRate*100,qpsRate*1100,qpsRate*100):
     qps.append(int(i))
     for j in range(0,5):
-        qpsbar.append(int(i)+j*5)
+        qpsbar.append(int(i)+j*5*qpsRate)
         dropedreq.append(int(i)*30-totalreq[l])
         l+=1
 #qps.pop()
@@ -104,10 +110,10 @@ for i in range(0,10):
 ##################################################################################################
 
 fig, ax = plt.subplots(figsize = (10, 5))
-plt.title('qps/latency for 30')
+plt.title('qps/latency for 30s')
 
 ax.set_xlabel('qps', color = 'r')
-ax.set_ylabel('latency in seconds', color = 'g')
+ax.set_ylabel('latency in milliseconds', color = 'g')
 
 #ax2 = ax.twinx()
 
@@ -120,10 +126,12 @@ z = AvLatency
 q = reqpersec
 
 
-ax.plot(qps, TailLatencyAv, marker="o", markersize=4, markeredgecolor="green", markerfacecolor="green")
-ax.plot(qps, AvLatencyAv, marker="o", markersize=4, markeredgecolor="black", markerfacecolor="black")
+ax.plot(qps, TailLatencyAv, marker="o", markersize=4, markeredgecolor="green", markerfacecolor="green", label="qps/tail latency (99th)")
+ax.plot(qps, AvLatencyAv, marker="o", markersize=4, markeredgecolor="black", markerfacecolor="black", label="qps/average latency")
 ax.bar(qpsbar, TailLatency, width=5.0, edgecolor = "black")
 ax.bar(qpsbar, AvLatency, width=5.0, edgecolor = "black")
+ax.legend(loc="upper left")
+
 plt.show()
 
 
@@ -132,10 +140,10 @@ plt.show()
 ##################################################################################################
 
 fig, ax = plt.subplots(figsize = (10, 5))
-plt.title('qps/latency for 30')
+plt.title('qps/latency for 30s')
 
 ax.set_xlabel('qps', color = 'r')
-ax.set_ylabel('latency in seconds', color = 'g')
+ax.set_ylabel('latency in milliseconds', color = 'g')
 
 #ax2 = ax.twinx()
 
@@ -146,10 +154,12 @@ x = qps[0:10]
 #x = np.array(x)
 y = TailLatency[0:10]
 z = AvLatency[0:10]
-ax.plot(qps[0:2], TailLatencyAv[0:2], marker="o", markersize=4, markeredgecolor="green", markerfacecolor="green")
-ax.plot(qps[0:2], AvLatencyAv[0:2], marker="o", markersize=4, markeredgecolor="black", markerfacecolor="black")
-ax.bar(qpsbar[0:10], TailLatency[0:10], width=5.0, edgecolor = "black")
-ax.bar(qpsbar[0:10], AvLatency[0:10], width=5.0, edgecolor = "black")
+ax.plot(qps[0:8], TailLatencyAv[0:8], marker="o", markersize=4, markeredgecolor="green", markerfacecolor="green", label="qps/tail latency (99th)")
+ax.plot(qps[0:8], AvLatencyAv[0:8], marker="o", markersize=4, markeredgecolor="black", markerfacecolor="black", label="qps/average latency")
+ax.bar(qpsbar[0:40], TailLatency[0:40], width=5.0, edgecolor = "black")
+ax.bar(qpsbar[0:40], AvLatency[0:40], width=5.0, edgecolor = "black")
+ax.legend(loc="upper left")
+
 plt.show()
 
 
@@ -157,7 +167,7 @@ plt.show()
 ##################################################################################################
 # qps/reqpersec/drops All  #
 ##################################################################################################
-
+"""
 x = qps
 w = totalreq
 d = dropedreq
@@ -173,10 +183,13 @@ ax2.set_ylabel('dropped requests', color = 'g')
 plt.rcParams["figure.figsize"] = [17.00, 13.50]
 plt.rcParams["figure.autolayout"] = True
 
-ax.plot(qps, totalreqAv, marker="o", markersize=4, markeredgecolor="black", markerfacecolor="black")
-ax2.plot(qps, dropedreqAv, marker="o", color="orange", markersize=2, markeredgecolor="red", markerfacecolor="purple")
+ax.plot(qps, totalreqAv, marker="o", markersize=4, markeredgecolor="black", markerfacecolor="black", label="qps/total requests")
+ax2.plot(qps, dropedreqAv, marker="o", color="orange", markersize=2, markeredgecolor="red", markerfacecolor="purple", label="qps/dropped requests")
 ax.bar(qpsbar, totalreq, width=5.0, edgecolor = "black")
 ax2.bar(qpsbar, dropedreq, color="orange", width=5.0, edgecolor = "black")
+ax.legend(loc="upper left")
+ax2.legend(loc="upper left")
+
 
 plt.show()
 
@@ -193,14 +206,17 @@ ax2.set_ylabel('dropped requests', color = 'g')
 plt.rcParams["figure.figsize"] = [17.00, 13.50]
 plt.rcParams["figure.autolayout"] = True
 
-ax.plot(qps[0:3], totalreqAv[0:3], marker="o", markersize=4, markeredgecolor="black", markerfacecolor="black")
-ax2.plot(qps[0:3], dropedreqAv[0:3], marker="o", color="orange", markersize=2, markeredgecolor="red", markerfacecolor="purple")
+ax.plot(qps[0:3], totalreqAv[0:3], marker="o", markersize=4, markeredgecolor="black", markerfacecolor="black", label="qps/total requests")
+ax2.plot(qps[0:3], dropedreqAv[0:3], marker="o", color="orange", markersize=2, markeredgecolor="red", markerfacecolor="purple", label="qps/dropped requests")
 ax.bar(qpsbar[0:15], totalreq[0:15], width=5.0, edgecolor = "black")
 ax2.bar(qpsbar[0:15], dropedreq[0:15], color="orange", width=5.0, edgecolor = "black")
+ax.legend(loc="upper left")
+ax2.legend(loc="upper left")
+
 
 plt.show()
 
-
+"""
 
 
 
@@ -216,7 +232,7 @@ x = qps
 w = totalreq
 d = dropedreq
 fig, ax = plt.subplots(figsize = (10, 5))
-plt.title('qps/total requests/dropped requests for 30')
+plt.title('qps/total requests/dropped requests for 30s')
 
 ax.set_xlabel('qps', color = 'r')
 ax.set_ylabel('requests', color = 'g')
@@ -225,16 +241,18 @@ ax.set_ylabel('requests', color = 'g')
 plt.rcParams["figure.figsize"] = [17.00, 13.50]
 plt.rcParams["figure.autolayout"] = True
 
-ax.plot(qps, totalreqAv, marker="o", markersize=4, markeredgecolor="black", markerfacecolor="black")
-ax.plot(qps, dropedreqAv, marker="o", color="orange", markersize=2, markeredgecolor="red", markerfacecolor="purple")
+ax.plot(qps, totalreqAv, marker="o", markersize=4, markeredgecolor="black", markerfacecolor="black", label="qps/total executed requests")
+ax.plot(qps, dropedreqAv, marker="o", color="orange", markersize=2, markeredgecolor="red", markerfacecolor="purple", label="qps/total dropped requests")
 ax.bar(qpsbar, totalreq, width=5.0, edgecolor = "black")
 ax.bar(qpsbar, dropedreq, color="orange", width=5.0, edgecolor = "black")
+ax.legend(loc="upper left")
+
 
 plt.show()
 
 
 fig, ax = plt.subplots(figsize = (10, 5))
-plt.title('qps/total requests/dropped requests for 30')
+plt.title('qps/total requests/dropped requests for 30s')
 
 ax.set_xlabel('qps', color = 'r')
 ax.set_ylabel('requests', color = 'g')
@@ -243,9 +261,394 @@ ax.set_ylabel('requests', color = 'g')
 plt.rcParams["figure.figsize"] = [17.00, 13.50]
 plt.rcParams["figure.autolayout"] = True
 
-ax.plot(qps[0:3], totalreqAv[0:3], marker="o", markersize=4, markeredgecolor="black", markerfacecolor="black")
-ax.plot(qps[0:3], dropedreqAv[0:3], marker="o", color="orange", markersize=2, markeredgecolor="red", markerfacecolor="purple")
-ax.bar(qpsbar[0:15], totalreq[0:15], width=5.0, edgecolor = "black")
-ax.bar(qpsbar[0:15], dropedreq[0:15], color="orange", width=5.0, edgecolor = "black")
+ax.plot(qps[0:8], totalreqAv[0:8], marker="o", markersize=4, markeredgecolor="black", markerfacecolor="black", label="qps/total executed requests")
+ax.plot(qps[0:8], dropedreqAv[0:8], marker="o", color="orange", markersize=2, markeredgecolor="red", markerfacecolor="purple", label="qps/total dropped requests")
+ax.bar(qpsbar[0:40], totalreq[0:40], width=5.0, edgecolor = "black")
+ax.bar(qpsbar[0:40], dropedreq[0:40], color="orange", width=5.0, edgecolor = "black")
+ax.legend(loc="upper left")
 
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+for name in glob("./outputs/default_flags_smt_disabled_100-1000qps/cstateAv"):
+    f=open(name, "r")
+#f=open("cstate.txt", "r")
+data_array1 = []
+for row in csv.reader(f):
+    data_array1.append(row)
+    
+    #for item in row:
+        #item = item.split()
+        #data_array1.append(item)
+
+#print(data_array1)
+c0t = []
+c1t = []
+c1et = []
+c6t = []
+c0 = []
+c1 = []
+c1e = []
+c6 = []
+x = []
+j=0
+for i in range(len(data_array1)):
+    if(len(data_array1[i]) == 4):
+        if(float(str(data_array1[i][0]).replace("[","").replace(",",""))>1.00):
+            c0t.append(float(str(data_array1[i][0]).replace("[","").replace(",","")))
+            c1t.append(float(str(data_array1[i][1]).replace(",","")))
+            c1et.append(float(str(data_array1[i][2]).replace(",","")))
+            c6t.append(float(str(data_array1[i][3]).replace("]","")))
+        else:
+            x.append(int(int(j)/5+1)*qpsRate*100+j%5*5*qpsRate)
+            j+=1
+            c0.append(float(str(data_array1[i][0]).replace("[","").replace(",",""))*100.00)
+            c1.append(float(str(data_array1[i][1]).replace(",",""))*100.00)
+            c1e.append(float(str(data_array1[i][2]).replace(",",""))*100.00)
+            tem=float(str(data_array1[i][3]).replace("]",""))
+            if (tem>=0):
+                c6.append(tem*100.00)
+            else :
+                c6.append(0)
+                t=c0.pop(len(c0)-1)
+                c0.append(float(t)*(1.00+tem))
+                t=c1.pop(len(c0)-1)
+                c1.append(float(t)*(1.00+tem))
+                t=c1e.pop(len(c0)-1)
+                c1e.append(float(t)*(1.00+tem))
+        
+        
+#print(len(data_array1[0]))
+print(len(c0), len(c1), len(c1e), len(c6), "\n")
+#print("c0:",  c0, "\n\nc1:", c1, "\n\nc1e:", c1e, "\n\nc6:", c6)"
+print((x))
+
+# data from https://allisonhorst.github.io/palmerpenguins/
+
+# create data
+
+y1 = np.array(c0)
+y2 = np.array(c1)
+y3 = np.array(c1e)
+y4 = np.array(c6)
+ 
+# plot bars in stack manner
+plt.bar(x, y1, color='r', width=qpsRate)
+plt.bar(x, y2, bottom=y1, color='b', width=qpsRate)
+plt.bar(x, y3, bottom=y1+y2, color='y', width=qpsRate)
+plt.bar(x, y4, bottom=y1+y2+y3, color='g', width=qpsRate)
+plt.xlabel("qps")
+plt.ylabel("Time percentage(%) out of 30 sec in each C-state")
+plt.legend(['c0', 'c1', 'c1e', 'c6'])
+plt.title("Time percentage(%) out of 30 sec, in each C-state relative to qps")
+plt.show()
+
+y1 = np.array(c0t)
+y2 = np.array(c1t)
+y3 = np.array(c1et)
+y4 = np.array(c6t)
+ 
+# plot bars in stack manner
+plt.bar(x, y1, color='r', width=qpsRate)
+plt.bar(x, y2, bottom=y1, color='b', width=qpsRate)
+plt.bar(x, y3, bottom=y1+y2, color='y', width=qpsRate)
+plt.bar(x, y4, bottom=y1+y2+y3, color='g', width=qpsRate)
+plt.xlabel("qps")
+plt.ylabel("CState transitions in 30 sec in each C-state")
+plt.legend(['c0', 'c1', 'c1e', 'c6'])
+plt.title("CState transitions in 30 sec, in each C-state relative to qps")
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import numpy as np
+import csv
+import matplotlib.pyplot as plt
+import pandas as pd
+from glob import glob
+
+qpsRate=1
+
+
+
+
+for name in glob("./outputs/default_flags_smt_disabled_100-1000qps/cpuCState"):
+    f=open(name, "r")
+#f=open("cstate.txt", "r")
+data_array1 = []
+for row in csv.reader(f):
+    data_array1.append(row)
+    
+    #for item in row:
+        #item = item.split()
+        #data_array1.append(item)
+
+#print(data_array1)
+c0t = []
+c1t = []
+c1et = []
+c6t = []
+c0 = []
+c1 = []
+c1e = []
+c6 = []
+x = []
+
+tc0t = [0] * 200
+tc1t = [0] * 200
+tc1et = [0] * 200
+tc6t = [0] * 200
+tc0 = [0] * 200
+tc1 = [0] * 200
+tc1e = [0] * 200
+tc6 = [0] * 200
+j=0
+
+
+print(len(data_array1))
+for i in range(0, len(data_array1), 10):
+    print(j)
+    for q in range(0,10):
+        if(len(data_array1[i+q]) == 4):
+            if(float(str(data_array1[i+q][0]).replace("[","").replace(",",""))>1.00):
+                tc0t[j]+=(float(str(data_array1[i+q][0]).replace("[","").replace(",","")))
+                tc1t[j]+=(float(str(data_array1[i+q][1]).replace(",","")))
+                tc1et[j]+=(float(str(data_array1[i+q][2]).replace(",","")))
+                tc6t[j]+=(float(str(data_array1[i+q][3]).replace("]","")))
+            else:
+                tc0[j]+=(float(str(data_array1[i+q][0]).replace("[","").replace(",","")))
+                tc1[j]+=(float(str(data_array1[i+q][1]).replace(",","")))
+                tc1e[j]+=(float(str(data_array1[i+q][2]).replace(",","")))
+                tem=float(str(data_array1[i+q][3]).replace("]",""))
+                if (tem>=0):
+                    tc6[j]=(tem)
+                else :
+                    tc6[j]=(0)
+                    tc0[j]-=(float(str(data_array1[i+q][0]).replace("[","").replace(",","")))
+                    tc1[j]-=(float(str(data_array1[i+q][1]).replace("[","").replace(",","")))
+                    tc1e[j]-=(float(str(data_array1[i+q][2]).replace("[","").replace(",","")))
+                    t=(float(str(data_array1[i+q][0]).replace("[","").replace(",","")))
+                    tc0[j]=(float(t)*(1.00+tem))
+                    t=(float(str(data_array1[i+q][1]).replace("[","").replace(",","")))
+                    tc1[j]=(float(t)*(1.00+tem))
+                    t=(float(str(data_array1[i+q][2]).replace("[","").replace(",","")))
+                    tc1e[j]=(float(t)*(1.00+tem))    
+    c0t.append(tc0t[j]/5)
+    c1t.append(tc1t[j]/5)
+    c1et.append(tc1et[j]/5)
+    c6t.append(tc6t[j]/5)
+    c0.append(tc0[j]*(100/(tc0[j]+tc1[j]+tc1e[j]+tc6[j])))
+    c1.append(tc1[j]*(100/(tc0[j]+tc1[j]+tc1e[j]+tc6[j])))
+    c1e.append(tc1e[j]*(100/(tc0[j]+tc1[j]+tc1e[j]+tc6[j])))
+    c6.append(tc6[j]*(100/(tc0[j]+tc1[j]+tc1e[j]+tc6[j])))
+    x.append(int(int(j)/20+1)*qpsRate*100+j%20*4*qpsRate)
+    j+=1
+        
+        
+#print(len(data_array1[0]))
+print(len(c0), len(c1), len(c1e), len(c6), "\n")
+#print("c0:",  c0, "\n\nc1:", c1, "\n\nc1e:", c1e, "\n\nc6:", c6)"
+print((x))
+
+# data from https://allisonhorst.github.io/palmerpenguins/
+
+# create data
+
+y1 = np.array(c0)
+y2 = np.array(c1)
+y3 = np.array(c1e)
+y4 = np.array(c6)
+ 
+# plot bars in stack manner
+plt.bar(x, y1, color='r', width=qpsRate*2)
+plt.bar(x, y2, bottom=y1, color='b', width=qpsRate*2)
+plt.bar(x, y3, bottom=y1+y2, color='y', width=qpsRate*2)
+plt.bar(x, y4, bottom=y1+y2+y3, color='g', width=qpsRate*2)
+plt.xlabel("qps")
+plt.ylabel("Time percentage(%) out of 30 sec in each C-state")
+plt.legend(['c0', 'c1', 'c1e', 'c6'])
+plt.title("Time percentage(%) out of 30 sec, in each C-state relative to qps")
+plt.show()
+
+y1 = np.array(c0t)
+y2 = np.array(c1t)
+y3 = np.array(c1et)
+y4 = np.array(c6t)
+ 
+# plot bars in stack manner
+plt.bar(x, y1, color='r', width=qpsRate*2)
+plt.bar(x, y2, bottom=y1, color='b', width=qpsRate*2)
+plt.bar(x, y3, bottom=y1+y2, color='y', width=qpsRate*2)
+plt.bar(x, y4, bottom=y1+y2+y3, color='g', width=qpsRate*2)
+plt.xlabel("qps")
+plt.ylabel("CState transitions in 30 sec in each C-state")
+plt.legend(['c0', 'c1', 'c1e', 'c6'])
+plt.title("CState transitions in 30 sec, in each C-state relative to qps")
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+for name in glob("./outputs/default_flags_smt_disabled_100-1000qps/power"):
+    f=open(name, "r")
+#f=open("power.txt", "r")
+#next(f)
+#next(f)
+#next(f)
+data_array2 = []
+for row in csv.reader(f):
+    data_array2.append(row)
+    
+    #for item in row:
+        #item = item.split()
+        #data_array2.append(item)
+
+#print(data_array2)
+
+dram = []
+package0 = []
+package1 = []
+
+for i in range(len(data_array2)):
+    if (len(package1)==50):
+        break
+    if(data_array2[i][0]=="dram"):
+        t1=float(data_array2[i+1][1])-float(data_array2[i+2][1])
+        t2=float(data_array2[i+1][0])-float(data_array2[i+2][0])
+        dram.append(t1/t2/1000000)
+    if(data_array2[i][0] == "package-0"):
+        t1=float(data_array2[i+1][1])-float(data_array2[i+2][1])
+        t2=float(data_array2[i+1][0])-float(data_array2[i+2][0])
+        package0.append(t1/t2/1000000)
+    if(data_array2[i][0] == "package-1"):
+        t1=float(data_array2[i+1][1])-float(data_array2[i+2][1])
+        t2=float(data_array2[i+1][0])-float(data_array2[i+2][0])
+        package1.append(t1/t2/1000000)
+
+
+#print(len(dram), len(package0), len(package1), "\n")
+#print("dram:",  dram, "\n\npackage0:", package0, "\n\npackage1:", package1)
+
+
+y1 = np.array(dram)
+y2 = np.array(package0)
+y3 = np.array(package1)
+ 
+# plot bars in stack manner
+plt.bar(x, y1, color='r', width=qpsRate)
+plt.bar(x, y2, bottom=y1, color='b', width=qpsRate)
+plt.bar(x, y3, bottom=y1+y2, color='y', width=qpsRate)
+plt.xlabel("qps")
+plt.ylabel("Power consumption in W")
+plt.legend(['dram', 'package0', 'package1'])
+plt.title("Power consumption in W relative to qps")
 plt.show()
--- run_user.sh
+++ run_user.sh
@@ -1,7 +1,10 @@
-
 #!/bin/bash
 o=$1
-rm runData
+DIR="./output"
+if [ ! -d "$DIR" ]; then
+  mkdir ./output
+  echo "creating output dir"
+fi
 for (( c=0 ; c<$1 ; c++ ));
 do
         for (( i=0 ; i<5 ; i++ ));
@@ -12,7 +15,7 @@ do
                         echo $d
                 #./wrk2/wrk -D exp -t 2 -c 2 -d 30 -s ./wrk2/scripts/social-network/compose-post.lua http://localhost:8080/wrk2-api/post/compose -R $d >> runData
                         sudo python3 ./profiler/profiler.py -n node0 start
-                        ./wrk2/wrk -D exp -t 2 -c 2 -d 30 -s ./wrk2/scripts/social-network/read-home-timeline.lua http://localhost:8080/wrk2-api/home-timeline/read -R $d >> output/runData
+                        ./wrk2/wrk -D exp -t 2 -c 2 -d 30 -s ./wrk2/scripts/social-network/read-user-timeline.lua http://localhost:8080/wrk2-api/user-timeline/read -R $d >> output/runData
                         sudo python3 ./profiler/profiler.py -n node0 stop
                         sudo python3 ./profiler/profiler.py -n node0 report -d ~/temp/node0/qps$d/repeat$i
        # ./wrk2/wrk -D exp -t 2 -c 2 -d $3 -L -s ./wrk2/scripts/social-network/mixed-workload.lua http://localhost:8080/wrk2-api/post/compose -R $2 >> file1
@@ -32,12 +35,17 @@ for (( c=0 ; c<$1 ; c++ ));
 do
         for (( i=0 ; i<5 ; i++ ));
         do
-                d=$(($(($c+1))*100))
-				python3 ./profiler/analyze.py ~/temp/node0/qps$d/repeat$i/ 0 19 30 >> output/cstateAv
+                d=$(($(($c+1))*100
+				python3 ./profiler/analyze.py ~/temp/node0/qps$d/repeat$i/ 0 20 30 >> output/cstateAv
 				cat ~/temp/node0/qps$d/repeat$i/dram >> output/power
 				cat ~/temp/node0/qps$d/repeat$i/package-0 >> output/power
 				cat ~/temp/node0/qps$d/repeat$i/package-1 >> output/power
 				echo '-----' >> output/power
+				for (( j=0 ; j<20 ; j++ ));
+				do
+                                                l=$(($j+1))
+						python3 ./profiler/analyze.py ~/temp/node0/qps$d/repeat$i/ $j $l 30 >> output/cpuCState
+				done
 
         done
 		echo 'repeat end' >> output/cstateAv
