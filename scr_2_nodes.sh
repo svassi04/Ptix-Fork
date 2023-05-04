c6aeca8985534e815336d73eb1001941607b4874
Stylianos Vassiliou <63289585+svassi04@users.noreply.github.com>
2023-05-04 03:41:11 +0300

    fix

--- scr_2_nodes.sh
+++ scr_2_nodes.sh
@@ -90,7 +90,7 @@ sudo docker stack deploy --compose-file=docker-compose-swarm-2-nodes.yml SocialN
 cd Ptix-Fork;
 chmod +x scr_work.sh;
 yes Y|./scr_work.sh;
-sudo$variable"
+sudo$variable
 #git clone https://github.com/hvolos/profiler.git
 #python3 scripts/init_social_graph.py --graph=socfb-Reed98;
 #cd wrk2;
