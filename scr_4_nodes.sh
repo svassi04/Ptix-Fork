ad391737de53578d9124638c197a53566bc2198f
Stylianos Vassiliou <63289585+svassi04@users.noreply.github.com>
2023-05-05 23:51:38 +0300

    4 nodes

---
+++ scr_4_nodes.sh
@@ -0,0 +1,53 @@
+#!/bin/bash 
+
+export NODE0=$(ssh node0 hostname)
+echo $NODE0
+export NODE1=$(ssh node1 hostname)
+echo $NODE1
+export NODE2=$(ssh node0 hostname)
+echo $NODE2
+export NODE3=$(ssh node1 hostname)
+echo $NODE3
+
+for (( i=0 ; i<$1 ; i++ )); 
+do
+
+    if [ $i -eq 0 ]; then
+	echo "off" | sudo tee /sys/devices/system/cpu/smt/control
+	chmod +x scr_master.sh
+	yes Y|./scr_master.sh
+	
+	variable=`cat file |  grep "docker swarm join --token"`
+else
+	
+	ssh node$i<<EOT
+	echo "forceoff" | sudo tee /sys/devices/system/cpu/smt/control
+
+	git clone https://github.com/svassi04/Ptix-Fork.git
+	cd Ptix-Fork
+	chmod +x scr_work.sh
+	yes Y|./scr_work.sh
+	sudo$variable
+EOT
+	
+	
+fi
+done
+sudo docker stack deploy --compose-file=docker-compose-swarm-4-nodes.yml SocialNetwork
+
+ssh  node$i "git clone https://github.com/svassi04/Ptix-Fork.git;
+cd Ptix-Fork;
+chmod +x scr_work.sh;
+yes Y|./scr_work.sh;
+sudo$variable"
+#git clone https://github.com/hvolos/profiler.git
+#python3 scripts/init_social_graph.py --graph=socfb-Reed98;
+#cd wrk2;
+#make;
+
+
+
+
+
+
+
